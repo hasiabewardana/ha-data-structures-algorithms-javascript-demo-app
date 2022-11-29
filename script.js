27305f05849502a4e71f7d71d28a9a222de76df4
Hasitha Abewardana <hasi.prasanga@gmail.com>
2022-11-29 16:52:26 +0000

    Increased data elements for findEmployee method to check the runtime.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Importing variables and functions
-import { emplyees, findEmployee } from './scripts/big-o/good-code.js'
+// import { emplyees, findEmployee } from './scripts/big-o/good-code.js'
+import { emplyees, moreEmplyees, allEmployees, findEmployee } from './scripts/big-o/scalability.js'
 
 // Calling functions
-findEmployee(emplyees);
+findEmployee(allEmployees);
