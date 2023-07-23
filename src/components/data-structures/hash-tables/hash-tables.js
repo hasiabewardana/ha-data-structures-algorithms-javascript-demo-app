fdfcdb3151ed9f4a9a243a27cc39c4af77c47473
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-23 13:15:05 +0530

    Implemented hash tables as objects in javascript.

---
+++ src/components/data-structures/hash-tables/hash-tables.js
@@ -0,0 +1,18 @@
+let user = {
+  age: 54,
+  name: 'Kylie',
+  magic: true,
+  scream: function() {
+    console.log('ahhhhhh!');
+  }
+};
+
+// console.log(user.age);
+
+// user.spell = 'abra kadabra';
+// console.log(user.spell);
+
+user.scream();
+
+//Map --> Gives you some order
+//Set --> No duplicate keys
